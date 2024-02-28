ca9a926620e67468c8ae701acf6012d5044c34b4
Lobanov Ivan <physic2003@mail.ru>
2024-02-29 00:00:00 +0300

    add image of trash and uri in api

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  # страница с услугами
     path('services/<int:service_id>/', views.service, name='service'),  # страница услуги
+    path('user/draft/', views.draft, name='draft') # cтраница черновика
 ]
